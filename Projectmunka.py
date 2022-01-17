2f137bb12bd6f59097858b214754c790e3517c62
viktorkaa <83224403+viktorkaa@users.noreply.github.com>
2022-01-17 16:14:32 +0100

    A munka kezdése+pontok alaphelyzete

--- Projectmunka.py
+++ Projectmunka.py
@@ -1,1 +1,12 @@
-print("majd itt lesz valami")
+import turtle
+import os
+
+wn = turtle.Screen()
+wn.title("Eniko és Viktor munkája")
+wn.bgcolor("black")
+wn.setup(width=800, height=600)
+wn.tracer(0)
+
+# Score
+score_a = 0
+score_b = 0
