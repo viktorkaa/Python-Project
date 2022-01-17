13c459e984ebde2cf72b0ffedf706bbeaa9ef3b6
enikonagy2005 <83341636+enikonagy2005@users.noreply.github.com>
2022-01-17 16:28:42 +0100

    Update Projectmunka.py

--- Projectmunka.py
+++ Projectmunka.py
@@ -36,3 +36,11 @@ ball.penup()
 ball.goto(0, 0)
 ball.dx = 2
 ball.dy = -2
+# Toll
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Player A: 0  Player B: 0", align="center", font=("Courier", 24, "normal"))
