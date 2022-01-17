479f189f55bac50c0abc08d457d9e89f03c3b0fc
enikonagy2005 <83341636+enikonagy2005@users.noreply.github.com>
2022-01-17 16:24:38 +0100

    Update Projectmunka.py

--- Projectmunka.py
+++ Projectmunka.py
@@ -10,3 +10,19 @@ wn.tracer(0)
 # Score
 score_a = 0
 score_b = 0
+# Elso szamu jatekos
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("white")
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)
+paddle_a.penup()
+paddle_a.goto(-350, 0)
+# Masodik szamu jatekos
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color("white")
+paddle_b.shapesize(stretch_wid=5, stretch_len=1)
+paddle_b.penup()
+paddle_b.goto(350, 0)
