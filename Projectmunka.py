ee97d8d470f333528e1d1f35488581ae8da29f5d
viktorkaa <83224403+viktorkaa@users.noreply.github.com>
2022-01-17 16:31:16 +0100

    A labda és az ütők mozgásai, méretei, valamint a  két ütő mozgásának kivitelezése billentyűzettel.

--- Projectmunka.py
+++ Projectmunka.py
@@ -36,3 +36,31 @@ ball.penup()
 ball.goto(0, 0)
 ball.dx = 2
 ball.dy = -2
+
+# Funkciók 
+def paddle_a_up():
+	y = paddle_a.ycor()
+	y += 20
+	paddle_a.sety(y)
+
+def paddle_a_down():
+	y = paddle_a.ycor()
+	y -= 20
+	paddle_a.sety(y)
+
+def paddle_b_up():
+	y = paddle_b.ycor()
+	y += 20
+	paddle_b.sety(y)
+
+def paddle_b_down():
+	y = paddle_b.ycor()
+	y -= 20
+	paddle_b.sety(y)
+
+# Irányítás a billentyűzettel
+wn.listen()
+wn.onkeypress(paddle_a_up, "w")
+wn.onkeypress(paddle_a_down, "s")
+wn.onkeypress(paddle_b_up, "Up")
+wn.onkeypress(paddle_b_down, "Down")
